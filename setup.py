ef99330ecd7d95773fe51460131889da160fae68
antoinecollet5 <antoine.collet5@gmail.com>
2022-07-22 16:58:18 +0200

    Update numpy version in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('CHANGELOG.rst') as history_file:
     history = history_file.read()
 
-requirements = ["numpy>=1.18.1"]
+requirements = ["numpy>=1.21"]
 
 setup_requirements = ['pytest-runner', 'wheel', ]
 
