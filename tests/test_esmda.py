0946091888b54656feac4ee81e724430abcfe5ea
antoinecollet5 <antoine.collet5@gmail.com>
2026-08-09 10:18:16 +0200

    FIX: by pass issue in test

--- tests/test_esmda.py
+++ tests/test_esmda.py
@@ -556,13 +556,9 @@ def test_esmda_exponential_case_batch(
     m_ensemble = np.stack((ma, mb), axis=0)
 
     # Observation error covariance matrix
-    if cov_obs_dim == 2:
-        cov_obs = covmats.CovViaCholesky(
-            sp.linalg.cholesky(np.diag(np.ones((obs.size,))), lower=True)
-        )
-    else:
-        cov_obs = covmats.CovViaDiagonal(np.ones((obs.size,)))
-
+    cov_obs = covmats.CovViaCholesky(
+        sp.linalg.cholesky(np.diag(np.ones((obs.size,))), lower=True)
+    )
     # Bounds on parameters (size m * 2)
     m_bounds = np.array([[0.0, 50.0], [-1.0, 1.0]])
 
